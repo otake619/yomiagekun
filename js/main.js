13855345a16bd6c47ca60dbaf41879a3a677df1d
otake619 <ryoheiotake@gmail.com>
2020-01-20 16:32:46 +0900

    finish!

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ audio.volume=0.2;
 
 //var music=new Audio();
 
-var playlist=['./sounds/1.mp3','./sounds/あ.mp3','./sounds/い.mp3','./sounds/う.mp3','./sounds/え.mp3','./sounds/お.mp3','./sounds/か.mp3','./sounds/き.mp3','./sounds/く.mp3','./sounds/け.mp3','./sounds/こ.mp3'];
+var playlist=['./sounds/1.mp3','./sounds/あ.mp3','./sounds/い.mp3','./sounds/う.mp3','./sounds/え.mp3','./sounds/お.mp3','./sounds/か.mp3','./sounds/き.mp3','./sounds/く.mp3','./sounds/け.mp3','./sounds/こ.mp3','./sounds/さ.mp3','./sounds/し.mp3','./sounds/す.mp3','./sounds/せ.mp3','./sounds/そ.mp3','./sounds/た.mp3','./sounds/ち.mp3','./sounds/つ.mp3','./sounds/て.mp3','./sounds/と.mp3','./sounds/な.mp3','./sounds/に.mp3','./sounds/ぬ.mp3','./sounds/ね.mp3','./sounds/の.mp3','./sounds/は.mp3','./sounds/ひ.mp3','./sounds/ふ.mp3','./sounds/へ.mp3','./sounds/ほ.mp3','./sounds/ま.mp3','./sounds/み.mp3','./sounds/む.mp3','./sounds/め.mp3','./sounds/も.mp3','./sounds/や.mp3','./sounds/ゆ.mp3','./sounds/よ.mp3','./sounds/ら.mp3','./sounds/り.mp3','./sounds/る.mp3','./sounds/れ.mp3','./sounds/ろ.mp3','./sounds/わ.mp3','./sounds/を.mp3','./sounds/ん.mp3','./sounds/が.mp3','./sounds/ぎ.mp3','./sounds/ぐ.mp3','./sounds/げ.mp3','./sounds/ご.mp3','./sounds/ざ.mp3','./sounds/じ.mp3','./sounds/ず.mp3','./sounds/ぜ.mp3','./sounds/ぞ.mp3','./sounds/だ.mp3','./sounds/ぢ.mp3','./sounds/づ.mp3','./sounds/で.mp3','./sounds/ど.mp3','./sounds/ば.mp3','./sounds/び.mp3','./sounds/ぶ.mp3','./sounds/べ.mp3','./sounds/ぼ.mp3','./sounds/ぱ.mp3','./sounds/ぴ.mp3','./sounds/ぷ.mp3','./sounds/ぺ.mp3','./sounds/ぽ.mp3',];
 
 //音声の連想配列("あいうえお"の50音順を昇順に数値として定義) 例：あ→１、ん→50
 var voiceDictionary={1:'あ',2:'い',3:'う',4:'え',5:'お',6:'か',7:'き',8:'く',9:'け',10:'こ',11:'さ',12:'し',13:'す',14:'せ',15:'そ',16:'た',17:'ち',18:'つ',19:'て',20:'と',21:'な',22:'に',23:'ぬ',24:'ね',25:'の',26:'は',27:'ひ',28:'ふ',29:'へ',30:'ほ',31:'ま',32:'み',33:'む',34:'め',35:'も',36:'や',37:'ゆ',38:'よ',39:'ら',40:'り',41:'る',42:'れ',43:'ろ',44:'わ',45:'を',46:'ん',47:'が',48:'ぎ',49:'ぐ',50:'げ',51:'ご',52:'ざ',53:'じ',54:'ず',55:'ぜ',56:'ぞ',57:'だ',58:'ぢ',59:'づ',60:'で',61:'ど',62:'ば',63:'び',64:'ぶ',65:'べ',66:'ぼ',67:'ぱ',68:'ぴ',69:'ぷ',70:'ぺ',71:'ぽ'};
@@ -30,6 +30,16 @@ function kakunin(){
     //textArray:textareaに入力された文字列を分割して配列としたもの
     textArray=inputText.split('');
     setNumberToWord();
+    //読み上げボタンが押されたので、リセットボタンを押せるようにする
+    document.getElementById("resetButton").disabled=false;
+    document.getElementById("inputButton").disabled=true;
+}
+
+//inputButtonのdisabledを切り替える関数(リセットボタンが押された際に呼ばれる)
+function changeDisabled(){
+    document.getElementById("inputButton").disabled=false;
+    //numbersOfWordの中身を空にする
+    numbersOfWord=[];
 }
 
 //1文字ずつvoiceDictionaryで定義した数値を当てはめていく
@@ -50,58 +60,10 @@ function setNumberToWord(){
 
 //setNumberToWord()で格納した数値のmp3ファイルを開く 1.mp3→'あ'の音声を再生、といった風に
 function playSound(){
-    //numbersOfWordから1つずつ数値を取り出して、数値に沿ったidの音声ファイルを開く
-//    for(var i=0,len=numbersOfWord.length;i<len;i++){
-////        audio.src=playlist[numbersOfWord[i]];
-////        audio.load();
-////        setTimeout(function(){
-////            audio.play();
-//        document.getElementById('1').play();
-////        },1000);
-////        console.log(audio.play());
-//        audio.currentTime=0;
-//        audio.src='';
-//    }
-
-//    a.play();
-//    a.addEventListener('ended',function(e){
-//        a.currentTime=0;
-//        a.play();
-//    });
-//    
-//    for(var i=0;i<numbersOfWord.length;i++){
-//        playlist[numbersOfWord[i]].play();
-//        playlist[numbersOfWord[i]].addEventListener('ended',function(e){
-//            playlist[numbersOfWord[i+1]].play();
-//        });
-//    }
     init();
 }
 
 function init(){
-//    audio.preload='auto';
-//    audio.src=playlist[numbersOfWord[0]];
-//    audio.load();
-//    audio.play();
-//    
-//    audio.addEventListener('ended',function(){
-//        for(var i=0;i<numbersOfWord.length;i++){
-//            audio.src=playlist[numbersOfWord[i]];
-//            audio.play();
-//        }
-//    });
-//    var index=0;
-//    audio.addEventListener('ended',function(){
-//        index++;
-//        if(index<playlist.length){
-//            audio.src=playlist[numbersOfWord];
-//            audio.play();
-//        }else{
-//            audio.src=playlist[0];
-//            audio.play();
-//            index=0;
-//        }
-//    });
     //numbersOfWordの一番最初の文字を格納(index=0)
     var index=0;
     audio.src=playlist[numbersOfWord[index]];
